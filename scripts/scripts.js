3a2d039e132b6a24030ab74a1c7b8eb85d79666f
Pavel3412 <pavel.dobrov.01@gmail.com>
2022-07-01 01:57:53 +0500

    init

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+const raft = document.getElementById('raft');
+const sup = document.getElementById('sup');
+
+raft.addEventListener('click', () => {
+    location.href = 'https://vk.com/9legendnn';
+});
+sup.addEventListener('click', () => {
+    location.href = 'https://vk.com/sup_9legend';
+})
